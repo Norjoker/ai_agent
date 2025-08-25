ee50575617e963fa67f4aa3d206661013b288bee
Stian <201008172+Norjoker@users.noreply.github.com>
2025-08-25 20:21:41 +0200

    added file reading

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -2,15 +2,20 @@ import os
 from config import MAX_CHARS
 
 def get_file_content(working_directory, file_path):
-    working_directory = os.path.abspath(working_directory)
-    full_path = os.path.abspath(os.path.join(working_directory, file_path))
-    #print(full_path)
-    #print(working_directory)
+    try:
+        working_directory = os.path.abspath(working_directory)
+        full_path = os.path.abspath(os.path.join(working_directory, file_path))
+        print(full_path)
+        print(working_directory)
 
-    if not os.path.isdir(os.path.abspath(full_path)):
-        return f'Error: File not found or is not a regular file: "{file_path}"'
-    elif os.path.commonpath([working_directory, full_path]) != working_directory:
-        return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(os.path.abspath(full_path)):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+        elif os.path.commonpath([working_directory, full_path]) != working_directory:
+            return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory'
+
+        with open(full_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            return file_content_string
     
-    with open(full_path, "r") as f:
-        file_content_strin = f.read(MAX_CHARS)
+    except Exception as e:
+        return f"Error: {e}"
--- tests.py
+++ tests.py
@@ -1,10 +1,10 @@
-from functions import get_files_info
+from functions import get_files_info, get_file_content
 # import functions.get_files_info
 
-test1 = get_files_info.get_files_info("calculator", ".")
-test2 = get_files_info.get_files_info("calculator", "pkg")
-test3 = get_files_info.get_files_info("calculator", "/bin")
-test4 = get_files_info.get_files_info("calculator", "../")
+test1 = get_file_content.get_file_content("calculator", "main.py")
+test2 = get_file_content.get_file_content("calculator", "pkg/calculator.py")
+test3 = get_file_content.get_file_content("calculator", "/bin/cat")
+test4 = get_file_content.get_file_content("calculator", ".pkg/does_not_exist.py")
 
 print(test1)
 print(test2)
